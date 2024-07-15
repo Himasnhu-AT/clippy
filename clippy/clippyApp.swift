d5309abe4f8ea64145896e376014bb39bf388e45
Himanshu-AT <aristech.official@gmail.com>
2024-07-15 19:59:00 +0530

    Initial Commit

---
+++ clippy/clippyApp.swift
@@ -0,0 +1,17 @@
+//
+//  clippyApp.swift
+//  clippy
+//
+//  Created by Himanshu on 7/15/24.
+//
+
+import SwiftUI
+
+@main
+struct clippyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
